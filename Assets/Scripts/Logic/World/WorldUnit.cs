1c6bc6b8dad7a545b4c16b53d089dc046dcc8dcd
FredShao <Fred@FredShaodeMacBook-Pro.local>
2016-01-21 01:47:40 +0800

    完善世界相关脚本

--- Assets/Scripts/Logic/World/WorldUnit.cs
+++ Assets/Scripts/Logic/World/WorldUnit.cs
@@ -5,24 +5,42 @@ namespace Argamente.Fight.Data
 {
     public class WorldUnit : AbstractWorldUnit
     {
-        /// <summary>
-        // 当前世界块的宽度和高度，（都是在原始世界块大小的基础上算出来的，大小永远为 (x * 2 + 1) * originWorldUnitSize
-        // 以保证最小区块中心点在世界(0.0)的位置
-        /// </summary>
-        //public float unit_width = 0.0f;
-        //public float unit_height = 0.0f;
 
         /// <summary>
-        ///  以当前区块大小为单元大小，距离原点的距离（肯定为整数）
+        /// 当前区块包含的原始区块数量
         /// </summary>
-        public int pos_x = 0;
-        public int pos_y = 0;
+        public int unit_width_count = 0;
+        public int unit_height_count = 0;
+
 
         /// <summary>
-        /// 当前区块距离远点的真实距离
+        /// 当前区块的真实宽度
         /// </summary>
-        //public float realPos_x = 0.0f;
-        //public float realPos_y = 0.0f;
+        /// <value>The width of the unit.</value>
+        public float unit_width {
+            get
+            {
+                return unit_width_count * originWorldUnitSize;
+            }
+        }
+
+        public float unit_height {
+            get
+            {
+                return unit_height_count * originWorldUnitSize;
+            }
+        }
+
+
+        // 当前区块大小，在父区块中的索引层，（区块总是正方形的）
+        public int unitIndexFromParentUnit = 0;
+
+
+        /// <summary>
+        /// 当前区块距离原点的真实距离，也就是当前区块的中心点
+        /// </summary>
+        public float realPos_x = 0.0f;
+        public float realPos_y = 0.0f;
 
 
         /// <summary>
@@ -56,110 +74,163 @@ namespace Argamente.Fight.Data
 
 
 
-        public void Create(bool isCreateNeighbourUnit)
+        public void Create (bool isCreateNeighbourUnit)
         {
+
+            if (!isCreateNeighbourUnit)
+            {
+                return;
+            }
+
+            // 创建八个方向相邻的区块
             if (left == null)
             {
-                left = new WorldUnit();
-                left.Create(false);
+                left = new WorldUnit ();
+                left.Create (false);
                 left.right = this;
             }
 
-            if(right == null)
+            if (right == null)
             {
-                right = new WorldUnit();
-                right.Create(false);
+                right = new WorldUnit ();
+                right.Create (false);
                 right.left = this;
             }
 
-            if(up == null)
+            if (up == null)
             {
-                up = new WorldUnit();
-                up.Create(false);
+                up = new WorldUnit ();
+                up.Create (false);
                 up.down = this;
             }
 
-            if(down == null)
+            if (down == null)
             {
-                down = new WorldUnit();
-                down.Create(false);
+                down = new WorldUnit ();
+                down.Create (false);
                 down.up = this;
             }
 
-            if(leftUp == null)
+            if (leftUp == null)
             {
-                leftUp = new WorldUnit();
-                leftUp.Create(false);
+                leftUp = new WorldUnit ();
+                leftUp.Create (false);
                 leftUp.rightDown = this;
             }
 
-            if(rightUp == null)
+            if (rightUp == null)
             {
-                rightUp = new WorldUnit();
-                rightUp.Create(false);
+                rightUp = new WorldUnit ();
+                rightUp.Create (false);
                 rightUp.leftDown = this;
             }
 
-            if(leftDown == null)
+            if (leftDown == null)
             {
-                leftDown = new WorldUnit();
-                leftDown.Create(false);
+                leftDown = new WorldUnit ();
+                leftDown.Create (false);
                 leftDown.rightUp = this;
             }
 
-            if(rightDown == null)
+            if (rightDown == null)
             {
-                rightDown = new WorldUnit();
-                rightDown.Create(false);
+                rightDown = new WorldUnit ();
+                rightDown.Create (false);
                 rightDown.leftUp = this;
             }
+        }
 
+
+        public void Destroy ()
+        {
+            if (left != null)
+            {
+                left.right = null;
+            }
+
+            if (right != null)
+            {
+                right.left = null;
+            }
+
+            if (up != null)
+            {
+                up.down = null;
+            }
+
+            if (down != null)
+            {
+                down.up = null;
+            }
+
+            if (leftUp != null)
+            {
+                leftUp.rightDown = null;
+            }
+
+            if (rightUp != null)
+            {
+                rightUp.leftDown = null;
+            }
+
+            if (leftDown != null)
+            {
+                leftDown.rightUp = null;
+            }
+
+            if (rightDown != null)
+            {
+                rightDown.leftUp = null;
+            }
+
+
+			
         }
 
 
 
 
-        public void Explore(Transform explorer)
+        public void Explore (Transform explorer)
         {
 
-            if(unitLevel == 0)
+            if (unitLevel == 0)
             {
-                ExploredCompletely();
+                ExploredCompletely ();
             }
             else
             {
-                ExploreSubUnits(explorer);
+                ExploreSubUnits (explorer);
             }
         }
 
 
-        private void ExploreSubUnits(Transform explorer)
+        private void ExploreSubUnits (Transform explorer)
         {
-            if(unExplorUnits != null)
+            if (unExplorUnits != null)
             {
-                for(int i = 0; i < unExplorUnits.Count; ++i)
+                for (int i = 0; i < unExplorUnits.Count; ++i)
                 {
-                    unExplorUnits[i].Explore(explorer);
+                    unExplorUnits [i].Explore (explorer);
                 }
             }
         }
 
 
-        private void ExploredCompletely()
+        private void ExploredCompletely ()
         {
             this.isExplored = true;
             this.isExploredCompletely = true;
-            this.RenderUnit(this.isExploredCompletely);
-            if(this.onExplored != null)
+            this.RenderUnit (this.isExploredCompletely);
+            if (this.onExplored != null)
             {
-                this.onExplored(this);
+                this.onExplored (this);
             }
         }
 
 
-        public override void RenderUnit(bool explored)
+        public override void RenderUnit (bool explored)
         {
-            base.RenderUnit(explored);
+            base.RenderUnit (explored);
 
         }
 
